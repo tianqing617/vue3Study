3e22d6b52e36a2cd10a94c1efd832774c4e399dd
刘天青 <liutianqing@dfsoft.com.cn>
2021-09-01 00:06:15 +0800

    ref

--- src/components/RefDemo.vue
+++ src/components/RefDemo.vue
@@ -1,17 +1,93 @@
 <template>
-  <div class="name">{{name}}</div>
+  <div class="name">
+    <p>
+      <span>First:</span>
+      <span>{{name.first}}</span>
+    </p>
+    <p>
+      <span>Second:</span>
+      <span>{{name.second}}</span>
+    </p>
+
+    <div class="control">
+      <button @click="setNewName">set name</button>
+    </div>
+  </div>
+
+  <hr />
+
+  <div class="name2">
+    <p>
+      <span>First2:</span>
+      <span>{{name2.first}}</span>
+    </p>
+    <p>
+      <span>Second2:</span>
+      <span>{{name2.second}}</span>
+    </p>
+
+    <div class="control">
+      <button @click="setNewName2">set name2</button>
+    </div>
+  </div>
+
+  <hr />
+
+  <div class="age">
+    <span>Age:</span>
+    <span>{{age}}</span>
+    <button @click="increaseAge">old</button>
+  </div>
+
 </template>
 
 <script>
-  import { ref } from 'vue'
+  import { ref, reactive } from 'vue'
 
   export default {
     name: 'RefDemo',
     setup() {
-      let name = ref('dog');
-      
+      // 1. 使用reactive方式
+      const name = reactive({
+        first: 'Liu',
+        second: 'Hero',
+      });
+
+      // 2. 使用ref方式
+      const name2 = ref({
+        first: 'Thomax',
+        second: 'Edison'
+      })
+
+      // 3. 非引用类型，只能使用ref
+      let age = ref(18);
+
+      console.log('name:', name);
+      console.log('name2:', name2);
+      console.log('age:', age);
+
+      // 4. 操作
+      function increaseAge() {
+        age.value += 1;
+      }
+
+      function setNewName() {
+        name.first = '老';
+        name.second = '刘';
+      }
+
+      function setNewName2() {
+        name2.value.first = '老';
+        name2.value.second = '王';
+      }
+
       return {
         name,
+        name2,
+        age,
+        setNewName,
+        setNewName2,
+        increaseAge,
       }
     }
   }
