eb608f567db6b2a2d7a5cae50e1bf00a3ca30428
刘天青 <liutianqing@dfsoft.com.cn>
2021-09-01 11:23:21 +0800

    use JSX

--- src/DynamicCpnt.vue
+++ src/DynamicCpnt.vue
@@ -5,12 +5,14 @@
   export default {
     name: 'DynamicCpnt',
     props:['currentCpnt'],
-    render() {
+    setup() {
       console.log(CpntList)
       if (this.currentCpnt) {
         return h(CpntList[this.currentCpnt]);
       } else {
         return h('div', 'No Component');
+        // return defineComponent({ render() { return <div>Test</div> }})
+        // return <div>No Components</div>
       }
     }
     
