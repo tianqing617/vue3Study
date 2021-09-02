72c7e78d66bfc63fc6db6a9ee9e3b01ee0e5081b
刘天青 <liutq@hollycrm.com>
2021-09-02 11:51:13 +0800

    feat: computed

--- src/components/ComputedDemo.vue
+++ src/components/ComputedDemo.vue
@@ -1,3 +1,64 @@
 <template>
-  <span>computed</span>
+  <div class="name">
+    <p>
+      <span>姓：</span>
+      <span>{{person.first}}</span>
+    </p>
+
+    <p>
+      <span>名：</span>
+      <span>{{person.second}}</span>
+    </p>
+    <p>
+      <span>全称：</span>
+      <span>{{person.fullname}}</span>
+    </p>
+  </div>
+  <hr>
+
+  <div class="input">
+    <p>
+      <span>全称：</span>
+      <input type="text" v-model="person.fullname">
+    </p>
+  </div>
 </template>
+
+<script>
+  import { reactive, computed } from 'vue';
+  export default {
+    name: 'ComputedDemo',
+    setup() {
+      const person = reactive({
+        first: 'Liu',
+        second: 'hero',
+      });
+
+      person.fullname = computed({
+        get() {
+          if (person.first) {
+            return person.first + '-' + person.second;
+          } else {
+            return '';
+          }
+        },
+        set(value) {
+          const nameArr = value.split('-');
+          console.log('value:', value, nameArr);
+
+          if (nameArr && nameArr.length === 2) {
+            person.first = nameArr[0];
+            person.second = nameArr[1];
+          } else {
+            person.first = '';
+            person.second = '';
+          }
+        }
+      })
+
+      return {
+        person,
+      }
+    },
+  }
+</script>
