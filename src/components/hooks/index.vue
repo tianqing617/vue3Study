037f8b72c748f90769e4c439cd03405ee74f6c26
刘天青 <liutq@hollycrm.com>
2021-09-03 14:39:40 +0800

    hooks

---
+++ src/components/hooks/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>当前点击时鼠标的坐标为：</h2>
+
+  <span>x：{{point.x}}</span>
+  <br/>
+  <span>y：{{point.y}}</span>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  import usePoint from '../hooks/usePoint'
+
+  export default {
+    name: 'HooksDemo',
+    setup(){
+      const point = usePoint()
+
+      return { point };
+    }
+  }
+</script>
+
