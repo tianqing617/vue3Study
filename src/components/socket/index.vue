d479eb92b3ac1ded8cee9445f93c9af34a9b6987
刘天青 <liutq@hollycrm.com>
2021-10-20 15:06:20 +0800

    socket

---
+++ src/components/socket/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>socket</span>
+</template>
+
+<script>
+  export default {
+    name: 'SocketDemo',
+    setup() {
+      console.log('test socket');
+
+      var ws = new WebSocket("wss://echo.websocket.org");
+
+      ws.onopen = function(evt) {
+        console.log("Connection open ..."); 
+        ws.send("Hello WebSockets!");
+      };
+
+      ws.onmessage = function(evt) {
+        console.log( "Received Message: " + evt.data);
+        ws.close();
+      };
+
+      ws.onclose = function(evt) {
+        console.log("Connection closed.");
+      };
+    }
+  }
+</script>
